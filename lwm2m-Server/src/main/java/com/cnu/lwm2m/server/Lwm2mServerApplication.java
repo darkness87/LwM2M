293df9afed1b6c4f15e92d21a826ba2d832c6276
LeePyeongBok <pblee@cnuglobal.com>
2020-09-29 15:50:29 +0900

    공통부 구현 #13
    1. application.properties가 적용되지 않는 문제 수정
     - default load에 대한 문제점은 확인되지 않음
    
    Signed-off-by: LeePyeongBok <pblee@cnuglobal.com>

--- lwm2m-Server/src/main/java/com/cnu/lwm2m/server/Lwm2mServerApplication.java
+++ lwm2m-Server/src/main/java/com/cnu/lwm2m/server/Lwm2mServerApplication.java
@@ -2,8 +2,10 @@ package com.cnu.lwm2m.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class Lwm2mServerApplication {
 
 	public static void main(String[] args) {
