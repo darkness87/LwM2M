9a976eac453d97844b383f8295bb14ce38af8e1d
sookwon <skchae@cnuglobal.com>
2020-10-08 15:07:22 +0900

    LwM2M Server CoAP 등록기능 #20 => Observe 기능 read 구현 - VO 추가

---
+++ lwm2m-Server/src/main/java/com/cnu/lwm2m/server/vo/ObserveDataVO.java
@@ -0,0 +1,14 @@
+package com.cnu.lwm2m.server.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ObserveDataVO {
+
+	private int id;
+	private String tid;
+	private String value;
+
+}
