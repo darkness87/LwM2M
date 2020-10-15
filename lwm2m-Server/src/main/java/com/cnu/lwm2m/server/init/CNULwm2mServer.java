9f367e7e1945b9bc70693a6403d6be442d9cd2e9
sookwon <skchae@cnuglobal.com>
2020-10-15 09:45:27 +0900

    공통부 구현 #13 => Server : Object 26249 추가

--- lwm2m-Server/src/main/java/com/cnu/lwm2m/server/init/CNULwm2mServer.java
+++ lwm2m-Server/src/main/java/com/cnu/lwm2m/server/init/CNULwm2mServer.java
@@ -29,7 +29,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CNULwm2mServer implements DisposableBean {
 	private final String[] KEPCO_MODELS = {"LWM2M_Server_1_Custom.xml", "LWM2M_Device_3_Custom.xml", "LWM2M_ConnectivityMonitoring_4_Custom.xml", "LWM2M_Location_6_Custom.xml", "LWM2M_ConnectivityStatistics_7_Custom.xml",
-			"26241.xml", "26243.xml", "26245.xml", "26247.xml"};
+			"26241.xml", "26243.xml", "26245.xml", "26247.xml", "26249.xml"};
 
 	private LeshanServer server;
 
