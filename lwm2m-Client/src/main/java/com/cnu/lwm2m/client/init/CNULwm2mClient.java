6f39064eadaa8cc8224b79a2f9bb772490f43513
LeePyeongBok <pblee@cnuglobal.com>
2020-10-29 16:12:24 +0900

    공통부 구현 #13
    1. OMA 커스터마이징 일부 오타 수정
    2. ip port properties로 위치 보정
    
    Signed-off-by: LeePyeongBok <pblee@cnuglobal.com>

--- lwm2m-Client/src/main/java/com/cnu/lwm2m/client/init/CNULwm2mClient.java
+++ lwm2m-Client/src/main/java/com/cnu/lwm2m/client/init/CNULwm2mClient.java
@@ -1,7 +1,6 @@
 package com.cnu.lwm2m.client.init;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 import org.eclipse.californium.core.network.config.NetworkConfig;
@@ -13,7 +12,8 @@ import org.eclipse.leshan.core.model.ObjectModel;
 import org.eclipse.leshan.core.model.StaticModel;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
@@ -35,9 +35,14 @@ import com.cnu.lwm2m.client.models.KepcoSoftware;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Component
 @Slf4j
+@Component
+@PropertySource("classpath:lwm2m.properties")
 public class CNULwm2mClient extends AbsCNUModelSettings implements DisposableBean {
+	private final String[] KEPCO_MODELS = { "LWM2M_Server_1_Custom.xml", "LWM2M_Device_3_Custom.xml",
+			"LWM2M_ConnectivityMonitoring_4_Custom.xml", "LWM2M_Location_6_Custom.xml",
+			"LWM2M_ConnectivityStatistics_7_Custom.xml", "26241.xml", "26243.xml", "26245.xml", "26247.xml",
+			"26249.xml" };
 
 	@Autowired
 	public ObjectExcuteTask task;
@@ -47,35 +52,23 @@ public class CNULwm2mClient extends AbsCNUModelSettings implements DisposableBea
 
 	private LeshanClient client;
 
+	@Value("${lwm2m.server.ip:leshan.eclipseprojects.io}")
+	private String serverIp;
+
+	@Value("${lwm2m.server.port:5683}")
+	private String serverPort;
+
 	public void run() {
 		// TODO: endpoint는 DCU_ID가 적정해보인다... 조율필요
 		String endpoint = "CNU0492010001";
 		LeshanClientBuilder builder = new LeshanClientBuilder(endpoint);
 		List<ObjectModel> models = null;
-
-		Resource resource = resourceLoader.getResource("classpath:models");
-		try {
-			File directory = resource.getFile();
-			log.debug(directory.getAbsolutePath());
-			log.debug("is directory : {}", directory.isDirectory());
-
-			if (directory.isDirectory()) {
-				for (String fileName : directory.list()) {
-					log.debug(fileName);
-				}
-
-				models = ObjectLoader.loadDefault();
-				models.addAll(ObjectLoader.loadDdfResources("/models", directory.list()));
-			} else {
-				log.error("is directory : {}", directory.isDirectory());
-			}
-		} catch (IOException e) {
-			log.error(e.getMessage(), e);
-		}
+		models = ObjectLoader.loadDefault();
+		models.addAll(ObjectLoader.loadDdfResources("/models", KEPCO_MODELS));
 
 		// Initialize model
 		ObjectsInitializer init = new ObjectsInitializer(new StaticModel(models));
-		init.setInstancesForObject(LwM2mID.SECURITY, CNUSecurity.kepcoKcmvp("coap://leshan.eclipseprojects.io:5683", this));
+		init.setInstancesForObject(LwM2mID.SECURITY, CNUSecurity.kepcoKcmvp("coap://" + serverIp + ":" + serverPort, this));
 		init.setInstancesForObject(LwM2mID.SERVER, new CNUServer(this, task));
 		init.setInstancesForObject(LwM2mID.DEVICE, new CNUDevice(this, task));
 		init.setInstancesForObject(LwM2mID.ACCESS_CONTROL, new CNUAccessControl(this, task));
